552965b19c067fdc1aa64e23f9841344c355c8b3
Vika Mikhlai <vikamikhlai@gmail.com>
2022-05-10 02:48:00 +0300

    feat: add input in textarea

--- index.js
+++ index.js
@@ -17,11 +17,20 @@ wrapper.append(keyWrapper);
 const keyRow = document.createElement('div');
 keyRow.className = "keyboard__row";
 keyWrapper.append(keyRow);
+let isCaps = false;
 
 function createKey(data) {
     const key = document.createElement('div');
     const ruSpan = document.createElement('span');
     const enSpan = document.createElement('span');
+
+    ruSpan.className = "ru";
+    enSpan.className = "en";
+
+    enSpan.classList.add('active');
+
+    key.setAttribute("keycode", data.keycode);
+
     for (let prop in data.en) {
         const span = document.createElement('span');
         span.className = prop;
@@ -29,42 +38,106 @@ function createKey(data) {
         span.innerHTML = data.en[prop];
     }
 
-    enSpan.firstChild.classList.add('active');
-
     for (let prop in data.ru) {
         const span = document.createElement('span');
         span.className = prop;
         ruSpan.append(span);
         span.innerHTML = data.ru[prop];
     }
+
+    
     if (data.type === "control") {
         key.className = "key " + data.name;
+        if (data.name === 'space') {
+            key.dataset.inputable = true;
+        }
     } else {
         key.className = "key";
+        key.dataset.inputable = true;
     }
-    ruSpan.className = "ru";
-    enSpan.className = "en active";
+
+    isCaps
+        ? enSpan.querySelector('.caseUp').classList.add('active')
+        : enSpan.querySelector('.caseDown').classList.add('active');
     key.append(ruSpan, enSpan);
     keyRow.append(key);
 }
 
 for (let i = 0; i < keysData.length; i++) {
-    createKey(keysData[i]); 
+    createKey(keysData[i]);
 }
 
 const buttons = document.querySelector('.keyboard__row');
 let buttonClick;
 
+function changeCase(isShift) {
+    document.querySelectorAll('.key').forEach(el => {
+        el.querySelector('.active .active').classList.toggle('active');
+        isCaps 
+            ? isShift
+                ? el.querySelector('.shiftcaps').classList.add('active')
+                : el.querySelector('.caseDown').classList.add('active')
+            : isShift
+                ? el.querySelector('.caseUp').classList.add('active')
+                : el.querySelector('.caseDown').classList.add('active')
+    })
+}
+
 function clickOnButton (event) {
-    if (event.target.closest('.key')) {
-        event.target.closest('.key').classList.add('click');
-        textarea.value += event.target.closest('.key').querySelector('.active .active').innerHTML;
+    const keyElement = event.target.closest('.key');
+    if (keyElement) {
+        keyElement.classList.add('click');
+        switch (keyElement.dataset.action) {
+            case "enter":
+                textarea.value += '\r\n';
+                break;
+            case "capslock":
+                isCaps = !isCaps;
+                changeCase(false)
+                break;
+            case "shift":
+                changeCase(true)
+                break;
+            case "ctrl":
+
+                break;
+            case "backspace":
+
+                break;
+            case "del":
+
+                break;
+            case "up":
+
+                break;
+            case "down":
+
+                break;
+            case "left":
+
+                break;
+            case "right":
+
+                break;
+            case "alt":
+
+                break;
+            case "tab":
+                textarea.value += '\t';
+                break;
+        }
+
+        if (keyElement.dataset.inputable) {
+            textarea.value += keyElement.querySelector('.active .active').innerHTML;
+        }
         if (buttonClick) {
             buttonClick.classList.remove('click');
         }
-        buttonClick = event.target.closest('.key');
+        buttonClick = keyElement;
     }
 }
 
 buttons.addEventListener('click', clickOnButton);
-
+addEventListener('keydown', event => {
+    console.log(event)
+});
